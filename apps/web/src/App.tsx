1223df8dbdcf01ef0cbdbf4b9723fe61c3b5db3b
TAKAKURA AKITO <takakura.akito@it-mirai-h.ibk.ed.jp>
2026-01-21 23:08:04 +0900

    Moved App.tsx to apps/web/src/

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,1 +1,12 @@
-// App.tsx content here
+// apps/web/src/App.tsx
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello Vite + React!</h1>
+        </div>
+    );
+}
+
+export default App;
